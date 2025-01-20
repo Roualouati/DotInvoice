6ce2746214368e5994ca0c257e6e91a1fba9c8c0
Roualouati <110421954+Roualouati@users.noreply.github.com>
2025-01-20 02:52:02 +0100

    edit link

--- app/action.ts
+++ app/action.ts
@@ -83,7 +83,10 @@ export async function createInvoice(prevState: any, formData: FormData) {
           amount:submission.value.total,
           currency:submission.value.currency as any,
         }),
-        "invoiceLink": `http://localhost:3000/api/invoice/${data.id}`,
+        invoiceLink:  process.env.NODE_ENV !== "production"
+      ? `http://localhost:3000/api/invoice/${data.id}`
+      : `https://https://dotinvoice.vercel.appp/api/invoice/${data.id}`,
+
       }
     });
     return redirect("/dashboard/invoices");
@@ -142,8 +145,11 @@ export async function editInvoice(prevState:any,formData: FormData) {
         amount:submission.value.total,
         currency:submission.value.currency as any,
       }),
-      "invoiceLink": `http://localhost:3000/api/invoice/${data.id}`,
-    }
+      invoiceLink:  process.env.NODE_ENV !== "production"
+      ? `http://localhost:3000/api/invoice/${data.id}`
+      : `https://https://dotinvoice.vercel.appp/api/invoice/${data.id}`,
+
+    },
   });
   return redirect("/dashboard/invoices");
 
