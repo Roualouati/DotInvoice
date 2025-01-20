18d4be0383b4cee674d3ba93d74992892c0a7c77
Roualouati <110421954+Roualouati@users.noreply.github.com>
2025-01-20 03:06:43 +0100

    edit link

--- app/action.ts
+++ app/action.ts
@@ -85,7 +85,7 @@ export async function createInvoice(prevState: any, formData: FormData) {
         }),
        invoiceLink:  process.env.NODE_ENV !== "production"
       ? `http://localhost:3000/api/invoice/${data.id}`
-      : `https//dotinvoice.vercel.appp/api/invoice/${data.id}`,
+      : `https://dotinvoice.vercel.appp/api/invoice/${data.id}`,
 
       }
     });
